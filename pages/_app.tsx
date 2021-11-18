44ca89d02905a6a3c39441f4bf0f3d0ac9eb7379
Parth Gandhi <parthgandhi@outlook.com>
2021-11-18 23:54:12 +0530

    setup posthog and dummy page to reproduce bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { usePostHog } from 'next-use-posthog'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const POSTHOG_API_KEY = process.env.NEXT_PUBLIC_POSTHOG_API_KEY;
+  // @ts-ignore
+  usePostHog(POSTHOG_API_KEY, { api_host: 'https://app.posthog.com' })
   return <Component {...pageProps} />
 }
 
